4dc40a2457ab777586dfb21e4057174c1ad61108
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-01-27 19:01:21 +0200

    внес мелкие правки

--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -2,8 +2,6 @@ import refs from './refs';
 import apiService from './apiService';
 import updateGalleryMarkup from './update-gallery-markup';
 
-// import { debounce } from 'lodash';
-
 export default function fetchImages() {
   refs.loadMoreBtn.classList.add('is-hidden');
 
@@ -15,9 +13,6 @@ export default function fetchImages() {
     refs.loadMoreBtn.classList.remove('is-hidden');
     updateGalleryMarkup(images);
 
-    // Как-то криво работает window.scrollTo().
-    // Часто скроллит до низа пока картинки не загрузились, в итоге потом остается место до которого не дошло.
-    // Хотел попробовать при помощи debounce отсрочить скролл, но не работает.
     window.scrollTo({
       top: document.documentElement.offsetHeight,
       behavior: 'smooth',
