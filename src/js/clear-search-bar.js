9621de719d17a1345d2bb0243a7034633d83b9b6
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-01-28 17:31:34 +0200

    сделал мелкие правки

--- src/js/clear-search-bar.js
+++ src/js/clear-search-bar.js
@@ -5,6 +5,7 @@ export default function clearSearchBar() {
   refs.input.value = '';
   apiService.query = refs.input.value;
   localStorage.setItem('input-value', refs.input.value);
+  refs.loadMoreBtn.classList.add('is-hidden');
   refs.gallery.innerHTML = '';
   refs.input.focus();
 }
