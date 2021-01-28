4d9e68175fca4bc03d6b14f948829e872c15bbbc
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-01-28 18:56:33 +0200

    сделал мелкие правки

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,7 +1,7 @@
 export default {
   body: document.querySelector('body'),
   form: document.querySelector('#search-form'),
-  input: document.querySelector('#search-form > input'),
+  input: document.querySelector('input'),
   clearBtn: document.querySelector('.clear-icon'),
   gallery: document.querySelector('.gallery'),
   loadMoreBtn: document.querySelector('.load-more-btn'),
